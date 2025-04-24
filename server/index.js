0e6102618950c998499138422ae882a74faa8acb
Bekzodmirzaaliyev <bekzodmirzaaliyev27@gmail.com>
2025-04-24 16:05:30 +0500

    ASD

--- server/index.js
+++ server/index.js
@@ -13,23 +13,46 @@ const io = new Server(server, {
   },
 });
 
-io.on("connection", (socket) => {
-  console.log("Yangi foydalanuvchi:", socket.id);
+const players = {}; // { socket.id: { name, room, hp } }
 
+io.on("connection", (socket) => {
   socket.on("join_room", ({ name, room }) => {
     socket.join(room);
-    console.log(`${name} xonaga kirdi: ${room}`);
+    players[socket.id] = { name, room, hp: 100 };
     socket.to(room).emit("player_joined", { id: socket.id, name });
-    socket.on("move", ({ room, position }) => {
-      socket.to(room).emit("player_move", { id: socket.id, position });
-    });
+  });
+
+  socket.on("move", ({ room, position }) => {
+    socket.to(room).emit("player_move", { id: socket.id, position });
+  });
+
+  socket.on("attack", ({ room }) => {
+    socket.to(room).emit("player_attacked", { id: socket.id });
+  });
+
+  socket.on("damage", ({ targetId, amount }) => {
+    if (players[targetId]) {
+      players[targetId].hp -= amount;
+      if (players[targetId].hp <= 0) {
+        io.to(players[targetId].room).emit("player_dead", targetId);
+        delete players[targetId];
+      } else {
+        io.to(players[targetId].room).emit("player_damaged", {
+          id: targetId,
+          hp: players[targetId].hp,
+        });
+      }
+    }
   });
 
   socket.on("disconnect", () => {
-    console.log("Foydalanuvchi chiqdi:", socket.id);
+    const player = players[socket.id];
+    if (player) {
+      socket.to(player.room).emit("player_left", socket.id);
+      delete players[socket.id];
+    }
   });
 });
-
 server.listen(3001, () => {
   console.log("Server 3001-portda ishlamoqda");
 });
